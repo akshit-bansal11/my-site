a2b786a481c63b28ef466bf49b6a8eb7b81c7e6c
akshit-bansal11 <artistbansal2004@gmail.com>
2025-08-27 01:49:24 +0530

    Add loader

--- src/pages/Colors.jsx
+++ src/pages/Colors.jsx
@@ -828,7 +828,11 @@ function ImageColorExtractor() {
                 </motion.div>
             )}
 
-            {isLoading && <div className="text-white">Extracting colors...</div>}
+            {isLoading &&
+                <div
+                    class="w-10 h-10 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"
+                ></div>
+            }
             {error && <div className="text-red-500">{error}</div>}
 
             {palettes && (
